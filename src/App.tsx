b478778dc84c90249c41c26203ed6839cd81661e
Yves Candau <ycandau@sfu.ca>
2021-10-19 23:42:23 -0700

    Add basic App components

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,17 @@ import './App.css';
 // Default export
 
 const App = () => {
-  return <main id="app">App</main>;
+  return (
+    <main id="app">
+      <section id="header">Header</section>
+      <section id="regex">Regex</section>
+      <section id="text">Text</section>
+      <section id="graph">Graph</section>
+      <section id="info">Info</section>
+      <section id="warn">Warn</section>
+      <section id="run">Run</section>
+    </main>
+  );
 };
 
 //------------------------------------------------------------------------------
