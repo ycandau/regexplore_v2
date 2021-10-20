753ba5ba0de21423f154c23718d9c2ebec3fe14e
Yves Candau <ycandau@sfu.ca>
2021-10-19 23:38:34 -0700

    Clean up source files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+//------------------------------------------------------------------------------
+// Imports
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//------------------------------------------------------------------------------
+// Default export
+
+const App = () => {
+  return <main id="app">App</main>;
+};
+
+//------------------------------------------------------------------------------
 
 export default App;
