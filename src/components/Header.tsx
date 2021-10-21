66f6b4e34fdef44244b373dd65cdb1598b5ad5c2
Yves Candau <ycandau@sfu.ca>
2021-10-20 19:04:52 -0700

    Style the initial header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,20 +3,23 @@
 
 import { ReactElement } from 'react';
 
+import './Header.css';
+
 //------------------------------------------------------------------------------
 // Default export
 
 const Header = (): ReactElement => {
   return (
-    <div id="header">
+    <header id="header">
       <div className="logo">RegExplore</div>
+      <div className="search"></div>
       <div className="link">Explore</div>
       <div className="link">Log in</div>
       <div className="avatar">
         <div>Y</div>
       </div>
       <div className="theme-toggle">Toggle</div>
-    </div>
+    </header>
   );
 };
 
